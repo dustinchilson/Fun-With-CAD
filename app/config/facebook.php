864f4e1b7d32cdf6b2fda0d03c27f6af60fb8706
Dustin Chilson <dustinchilson@gmail.com>
2011-03-18 15:36:37 -0400

    Added Config

---
+++ app/config/facebook.php
@@ -0,0 +1,14 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/config/facebook.php
+  */
+$config = array(
+  'Facebook' => array(
+  	'appId' => '175816909122478',
+    'apiKey' => '084b2fb0def444d8b779665575fa7af7',
+    'secret' => 'b0066762ba9c1497d5b32dfdb864c405',
+    'cookie' => true,
+  )
+);
+?>
